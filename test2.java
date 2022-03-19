57419cd9c887fdeba81e154931d0843c5ae3dec5
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-19 23:30:25 +0900

    [Add]練習4-13

--- test2.java
+++ test2.java
@@ -12,11 +12,29 @@ public class Exercise
       for(int i = 0; i < count; i++)
       System.out.println( i % 10 );
 
-      // 練習4-2
+      // 練習4-12
       System.out.println( "数値を入力して下さい" );
       int sum = 0;
       while( sum <= 100 )
         sum += Integer.parseInt( br.readLine() );
       System.out.println( "合計は " + sum );
+
+      // 練習4-13
+      int strike = 0;
+      int ball = 0;
+
+      while( strike < 3 && ball < 4 )
+      {
+      System.out.print( "ストライク=1 or ボール=2 ？" );
+
+      int judge = Integer.parseInt( br.readLine() );
+
+      if( judge == 1 )
+        strike++;
+      else if( judge == 2 )
+        ball++;
+      }
+
+      System.out.println( ball + "ボール," + strike + "ストライク" );
   }
 }
