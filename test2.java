fa9b605d3652b3a97d28e48cf34c693a9dc944bd
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-21 23:16:27 +0900

    [Add]練習4-15

--- test2.java
+++ test2.java
@@ -58,5 +58,20 @@ public class Exercise
 
       System.out.println( ball + "ボール," + strike + "ストライク" );
 
+      // 練習4-15
+      System.out.println( "数値を入力して下さい" );
+      int value = Integer.parseInt( br.readLine() );
+      int n;
+
+      for( n = 2 ; n <= ( value / 2 ) ; n++ ){
+          if( ( value % n ) == 0 )
+              break;
+      }
+
+      if( n > ( value / 2 ) )
+          System.out.println( value + "は素数です" );
+      else
+          System.out.println( value + "は素数ではありません" );
+
   }
 }
