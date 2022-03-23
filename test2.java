c2957da8ee2d6e7b42afafce45fc50d433615f5a
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-23 23:28:49 +0900

    [Add]練習4-17

--- test2.java
+++ test2.java
@@ -85,5 +85,15 @@ public class Exercise
           }
       }
 
-  }
+      // 練習4-17
+      // 九九の表
+      for( int p = 1 ; p <= 9 ; p++ )
+      {
+          for( int q = 1 ; q <= 9 ; q++ )
+              System.out.printf(" %2d", p * q );
+
+          System.out.println();
+      }
+
+        }
 }
