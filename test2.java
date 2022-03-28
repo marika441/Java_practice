0d172c5e275b6ad8cc229f8bc14e6806e08d340f
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-28 22:53:27 +0900

    [Add]練習4-21

--- test2.java
+++ test2.java
@@ -136,6 +136,22 @@ public class Exercise
               System.out.print( "$" );
 
           System.out.println();
-}
+      }
+
+      // 練習4-21
+      int size = Integer.parseInt( br.readLine() );
+
+      for( int i = 0 ; i < size ; i++ )
+      {
+          for( int j = 0 ; j < size ; j++ )
+          {
+              if( i == j || ( size - i - 1 ) == j )
+                  System.out.print( "X" );
+              else
+                  System.out.print( " " );
+          }
+
+          System.out.println( "" );
+      }
     }
 }
