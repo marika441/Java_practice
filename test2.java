94faaa6ac25da7846a06e4fb2c368c6c4f551c59
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-29 23:05:07 +0900

    [Add]練習4-22

--- test2.java
+++ test2.java
@@ -153,5 +153,19 @@ public class Exercise
 
           System.out.println( "" );
       }
+
+    // 練習4-22
+    int     n1 = 0;
+    int     n2 = 1;
+    int     n3 = n1 + n2;
+
+    while( n1 <= 1000 )
+        {
+            System.out.print( n1 + "," );
+
+            n1 = n2;
+            n2 = n3;
+            n3 = n1 + n2;
+        }
     }
 }
