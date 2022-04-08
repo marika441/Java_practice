b2b89d2b1ca851628fe2573ec9975be3f2e2376f
marika441 <92457238+marika441@users.noreply.github.com>
2022-04-08 22:52:17 +0900

    [Add]練習6-1

---
+++ test4.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Exercise
+{
+  public static void main( String[] args )
+  {
+    // 練習6-1
+      System.out.println( "3の2乗=" + pow2( 3 ) );
+  }
+
+  static int pow2( int n )
+  {
+      return n * n;
+  }
+}
