7acf77b6b8c76134ce4943f7098ea6cd0d95fc2f
marika441 <92457238+marika441@users.noreply.github.com>
2022-04-09 23:19:04 +0900

    [Add]練習6-2

--- test4.java
+++ test4.java
@@ -2,14 +2,25 @@ import java.io.*;
 
 public class Exercise
 {
+  // 練習6-1
   public static void main( String[] args )
   {
-    // 練習6-1
-      System.out.println( "3の2乗=" + pow2( 3 ) );
+    System.out.println( "3の2乗=" + pow2( 3 ) );
   }
 
   static int pow2( int n )
   {
       return n * n;
+  }
+
+  // 練習6-2
+  public static void main( String[] args )
+  {
+      System.out.println( "4と9の平均=" + average( 4, 9 ) );
+  }
+
+  static int average( int x, int y )
+  {
+      return ( x + y ) / 2;
   }
 }
