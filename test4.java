1242dde952f4bd0edc8039e3a15e1e1aeeea6752
marika441 <92457238+marika441@users.noreply.github.com>
2022-04-12 23:02:29 +0900

    [Add]練習6-5

--- test4.java
+++ test4.java
@@ -65,4 +65,25 @@ public class Exercise
           System.out.println();
       }
   }
+
+  // 練習6-5
+  public static void main( String[] args )
+  {
+      DrawTriangle2( 3, '%' );
+      System.out.println();
+      DrawTriangle2( 5, '#' );
+      System.out.println();
+      DrawTriangle2( 7, '@' );
+  }
+
+  static void DrawTriangle2( int size, char ch )
+  {
+      for( int i = 0 ; i < size ; i++ )
+      {
+          for( int j = 0 ; j <= i ; j++ )
+              System.out.print( ch );
+
+          System.out.println();
+      }
+  }
 }
