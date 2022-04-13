98385e34dcf86a5d13d2b90e75f25f45094cd898
marika441 <92457238+marika441@users.noreply.github.com>
2022-04-13 23:02:38 +0900

    [Add]練習6-6

--- test4.java
+++ test4.java
@@ -86,4 +86,19 @@ public class Exercise
           System.out.println();
       }
   }
+
+    // 練習6-6
+    public static void main( String[] args )
+    {
+        for( int i = 1 ; i <= 9 ; i++ )
+            KukuColumn( i );
+    }
+
+    static void KukuColumn( int num )
+    {
+        for( int i = 1 ; i <= 9 ; i++ )
+            System.out.printf( " %2d", num * i );
+
+        System.out.println();
+    }
 }
