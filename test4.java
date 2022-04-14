523ce796b10df00ea95910d835b5db19f669fc3a
marika441 <92457238+marika441@users.noreply.github.com>
2022-04-14 23:30:52 +0900

    [Add]練習6-7

--- test4.java
+++ test4.java
@@ -101,4 +101,24 @@ public class Exercise
 
         System.out.println();
     }
+
+    // 練習6-7
+    public static void main( String[] args )
+    {
+        for( int n = 10000 ; n < 10100 ; n++ )
+            if( IsPrimeNumber( n ) )
+                System.out.print( n + " " );
+    }
+
+    static boolean IsPrimeNumber( int num )
+    {
+        if( num <= 3 )
+            return true;
+
+        for( int i = 2 ; i <= ( num / 2 ) ; i++ )
+            if( ( num % i) == 0 )
+                return false;
+
+        return true;
+    }
 }
